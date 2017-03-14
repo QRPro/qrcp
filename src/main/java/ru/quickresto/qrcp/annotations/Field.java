271c9dbf3a71623f33334decc122b8b0f69e4424
wanderer <darthwanderer@mail.ru>
2017-03-14 22:31:44 +0300

    First

---
+++ src/main/java/ru/quickresto/qrcp/annotations/Field.java
@@ -0,0 +1,12 @@
+package ru.quickresto.qrcp.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.FIELD)
+public @interface Field {
+    String name() default "";
+}

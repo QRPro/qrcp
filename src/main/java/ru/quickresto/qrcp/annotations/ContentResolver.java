2201cc279d36d78a36ef1b2e950f99e3449d2ea5
wanderer <darthwanderer@mail.ru>
2017-03-14 23:05:26 +0300

    Rename annotation

--- src/main/java/ru/quickresto/qrcp/annotations/ContentResolver.java
+++ src/main/java/ru/quickresto/qrcp/annotations/ContentResolver.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
-public @interface Provider {
+public @interface ContentResolver {
     String url();
 }

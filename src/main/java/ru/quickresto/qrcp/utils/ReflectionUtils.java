2a87b18cbe48b84d2cbf57d96986c87b8ec46dc2
wanderer <darthwanderer@mail.ru>
2017-03-15 10:30:47 +0300

    invoke setter method

--- src/main/java/ru/quickresto/qrcp/utils/ReflectionUtils.java
+++ src/main/java/ru/quickresto/qrcp/utils/ReflectionUtils.java
@@ -2,12 +2,22 @@ package ru.quickresto.qrcp.utils;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
 
 public class ReflectionUtils {
 
     private ReflectionUtils() {
+    }
+
+    public static void invokeSetter(Object object, Field field, Object value) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = object.getClass().getMethod("set"+field.getName()
+                .replaceFirst(field.getName().substring(0, 1), field.getName()
+                        .substring(0, 1).toUpperCase()),field.getType());
+
+        method.invoke(object, value);
     }
 
     public static List<Field> getDeclaredColumnFields(Class<?> type, Class<? extends Annotation> annotationType) {

dfefe5af394d67969002739465d7ee09499fcfcf
wanderer <darthwanderer@mail.ru>
2017-03-15 10:45:39 +0300

    Utils

--- src/main/java/ru/quickresto/qrcp/utils/ReflectionUtils.java
+++ src/main/java/ru/quickresto/qrcp/utils/ReflectionUtils.java
@@ -7,9 +7,22 @@ import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
 
+import ru.quickresto.qrcp.annotations.ResolverEntity;
+import ru.quickresto.qrcp.annotations.ResolverField;
+
 public class ReflectionUtils {
 
     private ReflectionUtils() {
+    }
+
+    public static String getFieldDeclaredName(Class<?> type, String name) throws NoSuchFieldException {
+        Field field = type.getField(name);
+
+        if (field.isAnnotationPresent(ResolverField.class)) {
+            return field.getAnnotation(ResolverField.class).name();
+        }
+
+        return null;
     }
 
     public static void invokeSetter(Object object, Field field, Object value) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
@@ -20,20 +33,20 @@ public class ReflectionUtils {
         method.invoke(object, value);
     }
 
-    public static List<Field> getDeclaredColumnFields(Class<?> type, Class<? extends Annotation> annotationType) {
+    public static List<Field> getDeclaredColumnFields(Class<?> type) {
         List<Field> declaredColumnFields = Collections.emptyList();
 
         Field[] fields = type.getDeclaredFields();
 
         for (Field field : fields) {
-            if (field.isAnnotationPresent(annotationType)) {
+            if (field.isAnnotationPresent(ResolverEntity.class)) {
                 declaredColumnFields.add(field);
             }
         }
 
         Class<?> parentType = type.getSuperclass();
         if (parentType != null) {
-            declaredColumnFields.addAll(getDeclaredColumnFields(parentType, annotationType));
+            declaredColumnFields.addAll(getDeclaredColumnFields(parentType));
         }
 
         return declaredColumnFields;

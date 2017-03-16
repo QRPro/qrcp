895712d07dec38729e71907389b74380ffebed87
wanderer <darthwanderer@mail.ru>
2017-03-16 17:06:26 +0300

    Refactoring

--- src/main/java/ru/quickresto/qrcp/mapper/BaseResolverEntity.java
+++
@@ -1,8 +0,0 @@
-package ru.quickresto.qrcp.mapper;
-
-public abstract class BaseResolverEntity {
-
-    public void insert() throws IllegalAccessException, NoSuchFieldException {
-        Mapper.insert(this);
-    }
-}
